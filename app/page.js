79fa11afbfbc134645119a18229c165a696f01d6
Mikołaj Florkiewicz <mikolaj@florkiewicz.me>
2024-09-25 14:25:01 +0200

    PR review

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 
 import { Network, NodeConfig } from "lumina-node";
 
@@ -9,60 +9,70 @@ import { LuminaContext, LuminaContextProvider } from "./lumina";
 export default function Home() {
   return (
     <LuminaContextProvider>
-      <Page />
+      <main>
+        <Launcher />
+        <Stats />
+      </main>
     </LuminaContextProvider>
   );
 }
 
-function Page() {
+function Launcher() {
+  const node = useContext(LuminaContext);
+
+  return (
+    <>
+      {node ?
+        <button
+          onClick={async () => {
+            let config = NodeConfig.default(Network.Mocha);
+            console.log("bootnodes", config.bootnodes);
+            console.log("network", config.network);
+
+            console.log("current: ", node);
+            let result = await node.start(config);
+            console.log("running: ", result);
+          }}
+        >
+          Start
+        </button>
+        :
+        <span>Loading...</span>
+      }
+    </>
+  );
+}
+
+function Stats() {
   const node = useContext(LuminaContext);
 
   const [peerTrackerInfo, setPeerTrackerInfo] = useState();
   const [syncerInfo, setSyncerInfo] = useState();
 
-  const statsUpdateInterval = setInterval(() => {
-    const update = async () => {
-      if (node && (await node.isRunning())) {
-        setPeerTrackerInfo(await node.peerTrackerInfo());
-        setSyncerInfo(await node.syncerInfo());
-      }
-    };
-    update();
-  }, 1000);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const update = async () => {
+        if (node && await node.isRunning()) {
+          setPeerTrackerInfo(await node.peerTrackerInfo());
+          setSyncerInfo(await node.syncerInfo());
+        }
+      };
+      update();
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [node]);
 
-  return (
-    <main>
-      <button
-        onClick={async () => {
-          let config = NodeConfig.default(Network.Mocha);
-          console.log("bootnodes", config.bootnodes);
-          console.log("network", config.network);
-
-          console.log("current: ", node);
-          let result = await node.start(config);
-          console.log("running: ", result);
-        }}
-      >
-        Start
-      </button>
-
-      <Stats peerTrackerInfo={peerTrackerInfo} syncerInfo={syncerInfo} />
-    </main>
-  );
-}
-
-function Stats({ peerTrackerInfo, syncerInfo }) {
-  if (!peerTrackerInfo || !syncerInfo) {
-    return;
+  if (!peerTrackerInfo || !syncerInfo) { 
+    return; 
   }
 
   return (
     <div>
       <div>network head: {syncerInfo.subjective_head.toString()}</div>
       <div>
-        stored ranges:{" "}
+        stored headers:
         {syncerInfo.stored_headers.map((range) => {
-          return `${range.start}..${range.end}`;
+          return ` ${range.start}..${range.end}`;
         })}
       </div>
       <div>
