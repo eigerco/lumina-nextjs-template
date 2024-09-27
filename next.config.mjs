0ce799b5fd3a611dfffa984512d3eebf5762ce2c
Mikołaj Florkiewicz <mikolaj@florkiewicz.me>
2024-09-27 15:01:00 +0200

    feat: Switch to bundler wasm-pack target

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,39 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.module.rules.push({
-      rules: [
-        {
-          test: /\.wasm$/,
-          type: "asset/resource",
-        },
-      ],
-    });
-
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack },
+  ) => {
+    config.experiments = {
+      asyncWebAssembly: true,
+      topLevelAwait: true,
+      layers: true,
+    };
+    if (!dev && isServer) {
+      config.output.webassemblyModuleFilename = "chunks/[id]_[hash].wasm";
+      config.plugins.push(new WasmChunksFixPlugin());
+    }
     return config;
   },
-
   output: "export",
 };
+
+class WasmChunksFixPlugin {
+  apply(compiler) {
+    compiler.hooks.thisCompilation.tap("WasmChunksFixPlugin", (compilation) => {
+      compilation.hooks.processAssets.tap(
+        { name: "WasmChunksFixPlugin" },
+        (assets) =>
+          Object.entries(assets).forEach(([pathname, source]) => {
+            if (!pathname.match(/\.wasm$/)) return;
+            compilation.deleteAsset(pathname);
+            const name = pathname.split("/")[1];
+            const info = compilation.assetsInfo.get(pathname);
+            compilation.emitAsset(name, source, info);
+          }),
+      );
+    });
+  }
+}
 
 export default nextConfig;
